b89175cdc3743a5af5bd7c4c1fec3fef3789ff4f
omar essam khedr <101929126+OmarEssamKhedr@users.noreply.github.com>
2022-05-19 23:41:07 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+import First from "./screens/First";
+
+function App() {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+
+  return (
+    <div className="app">
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a
+              className="close-search"
+              // onClick={() => setShowSearchpage(!showSearchPage)}
+            >
+              Close
+            </a>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <First />
+      )}
+    </div>
+  );
+}
+
+export default App;
